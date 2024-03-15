949ed719026991c0132ff69a6bea9247c018785e
ERLEY <erley.1701920254@ucaldas.edu.co>
2024-03-14 19:20:11 -0500

    Restructuracion del codigo

--- App.py
+++ App.py
@@ -1,14 +1,18 @@
-from tkinter import Menu
-from streamlit_agraph import Node, Edge
-import os
+import sys
+from pathlib import Path
+
+# Añadir el directorio src al sys.path para acceder a los módulos dentro de src
+src_path = Path(__file__).parent / 'src'
+sys.path.append(str(src_path))
+
+from models.UIManager import UIManager
+from streamlit_agraph import Node, Edge, agraph, Config
 import streamlit as st
-import json
 import networkx as nx
 import matplotlib.pyplot as plt
-from streamlit_agraph import agraph, Node, Edge, Config
 import random
 
-
 if __name__ == "__main__":
-    app = Menu
-    app.run()
+    ui_manager = UIManager()
+    ui_manager.run()
+
